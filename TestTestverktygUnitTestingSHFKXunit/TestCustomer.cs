b7bec1fef16d016396b5e9cba9d27465d1466c10
Simon Haag <simonhaag1@hotmail.com>
2022-05-09 14:58:57 +0200

    Added class files for testing Account & Customer

---
+++ TestTestverktygUnitTestingSHFKXunit/TestCustomer.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using TestverktygUnitTestingSHFK;
+using System.Threading.Tasks;
+
+namespace TestTestverktygUnitTestingSHFKXunit
+{
+    public class TestCustomer
+    {
+
+    }
+}
