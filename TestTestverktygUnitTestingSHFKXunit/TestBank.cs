74b6fd4d05d51f19b9f6b2dd23d45d046c17626a
Simon Haag <simonhaag1@hotmail.com>
2022-05-09 14:21:17 +0200

    Add project files.

---
+++ TestTestverktygUnitTestingSHFKXunit/TestBank.cs
@@ -0,0 +1,110 @@
+using System;
+using Xunit;
+using TestverktygUnitTestingSHFK;
+using System.Collections.Generic;
+
+namespace TestTestverktygUnitTestingSHFKXunit
+{
+    public class TestBank
+    {
+        [Fact]
+        public void TestGetCustomers_DoesNotContainNullValues()
+        {
+            Bank bank = new();
+
+            bank.Load(@"C:\Users\simon\source\repos\InlamningsuppgiftUnitTestSHFK\TestverktygUnitTestingSHFK\data.txt");
+
+            List < Customer > customerList = bank.GetCustomers();
+
+            Assert.All(customerList, customer =>
+            Assert.All(customer.GetType().GetProperties(), attribute =>
+            Assert.NotNull(attribute.GetValue(customer)))
+            );
+
+
+            /*foreach (var cust in bank.GetCustomers())
+            {
+                Assert.NotNull(cust.firstName);
+                Assert.NotNull(cust.personalNumber);
+                Assert.NotNull(cust.surName);
+            }*/
+        }
+
+        [Fact]
+        public void TestGetCustomers_CheckNumberOfCustomers()
+        {
+            Bank bank = new();
+
+            bank.Load(@"C:\Users\simon\source\repos\InlamningsuppgiftUnitTestSHFK\TestverktygUnitTestingSHFK\data.txt");
+            int expected = 3;
+            int actual = bank.GetCustomers().Count;
+
+            Assert.Equal(expected, actual);
+        }
+
+        [Fact]
+        public void AddCustomer_IfReturnTrueWhenAddingCustomers()
+        {
+            Bank bank = new();
+            bank.Load(@"C:\Users\simon\source\repos\InlamningsuppgiftUnitTestSHFK\TestverktygUnitTestingSHFK\data.txt");
+            bool addedCustomer = bank.AddCustomer("Simon", "12345");
+            /*string expected = "Simon";
+            string actual = bank.GetCustomer("12345").firstName;
+            Assert.Equal(expected, actual);*/
+            Assert.True(addedCustomer);
+
+        }
+
+        [Fact]
+        public void AddCustomer_CheckIfPersonalnumberIsUnique()
+        {
+            Bank bank = new();
+            bank.Load(@"C:\Users\simon\source\repos\InlamningsuppgiftUnitTestSHFK\TestverktygUnitTestingSHFK\data.txt");
+            List<Customer> customerList = bank.GetCustomers();
+
+            bank.AddCustomer("Simon", "12345");
+            bank.AddCustomer("CJ", "12345");
+            string expected = "12345";
+
+            Assert.All(customerList, customer => Assert.NotEqual(expected, customer.personalNumber));
+        } 
+        
+        [Fact]
+        public void GetCustomerInfo_CheckIfFirstNameAndPersonalNumberAreCorrect()
+        {
+            Bank bank = new();
+            bank.Load(@"C:\Users\simon\source\repos\InlamningsuppgiftUnitTestSHFK\TestverktygUnitTestingSHFK\data.txt");
+            List<string> customerAccount = bank.GetCustomerInfo("19860107");
+
+            string[] expected = {"Linnea", "19860107" };
+
+            for (int i = 0; i < expected.Length; i++)
+            {
+                Assert.Equal(expected[i], customerAccount[i]);
+            }
+
+        }
+        [Fact]
+        public void GetCustomerInfo_CheckIfAccountNumberAndBalance_AreActualNumerics()
+        {
+            Bank bank = new();
+            bank.Load(@"C:\Users\simon\source\repos\InlamningsuppgiftUnitTestSHFK\TestverktygUnitTestingSHFK\data.txt");
+            List<string> customerAccount = bank.GetCustomerInfo("19860107");
+
+            int balanceIndex = customerAccount.Count - 1;
+            int accountNumber = customerAccount.Count - 3;
+
+            string[] accountDetails = 
+                { customerAccount[balanceIndex],
+                customerAccount[accountNumber] };
+
+            bool isANumber;
+
+            for (int i = 0; i < accountDetails.Length; i++)
+            {
+               // isANumber = 
+            }
+
+        }
+    }
+}
---
+++ TestverktygUnitTestingSHFK/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TestverktygUnitTestingSHFK
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("This is a list of all customers\n");
+
+            Bank bank = new Bank();
+            bank.Load(@"C:\Users\simon\source\repos\TestverktygUnitTestingSHFK\data.txt");
+            List<Customer> customerList = bank.GetCustomers();
+
+            foreach (Customer customer in customerList)
+            {
+                Console.WriteLine("This is a new customer \n");
+                foreach (var details in customer.GetType().GetProperties())
+                {
+                    Console.WriteLine(details.GetValue(customer));
+                }
+            }
+
+        }
+    }
+}
